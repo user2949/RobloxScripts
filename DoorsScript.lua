e5a5899ca817c8c06695f7bb2dbf0f3da5f1acf3
Darryl Alcander Lim <103663597+user2949@users.noreply.github.com>
2024-09-06 05:55:33 +0700

    Create DoorsScript.lua

---
+++ DoorsScript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/Python/main/Main.lua"))()
